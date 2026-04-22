a31fbce9bc79495dfffe21444fce87963544a525
BT_C09_24 <mobileappdev@bths.edu>
2026-04-22 13:28:32 -0400

    Initial Commit

---
+++ GitZhuo/GitZhuoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitZhuoApp.swift
+//  GitZhuo
+//
+//  Created by Student on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct GitZhuoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
