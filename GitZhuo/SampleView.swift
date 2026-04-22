435cbd1ac4adea180da17046418733197464aec7
BT_C09_24 <mobileappdev@bths.edu>
2026-04-22 13:37:04 -0400

    added the SampleView file

---
+++ GitZhuo/SampleView.swift
@@ -0,0 +1,18 @@
+//
+//  SampleView.swift
+//  GitZhuo
+//
+//  Created by Student on 4/22/26.
+//
+
+import SwiftUI
+
+struct SampleView: View {
+    var body: some View {
+        Text("look at this sample view")
+    }
+}
+
+#Preview {
+    SampleView()
+}
