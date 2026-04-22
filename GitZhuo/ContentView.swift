facd345ccbf021aacd009f57267018f923c6275c
BT_C09_24 <mobileappdev@bths.edu>
2026-04-22 13:50:10 -0400

    modify + delete

--- GitZhuo/ContentView.swift
+++ GitZhuo/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "flag")
+            Image(systemName: "flags")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Fun with Git")
+            Text("Okshi with Git")
         }
         .padding()
     }
