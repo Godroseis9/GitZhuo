43093f61f867ab004fa8b1f254a5c8cd161a4c21
BT_C09_24 <mobileappdev@bths.edu>
2026-04-23 13:32:19 -0400

    Added struct to the Hello file

--- GitZhuo/Hello.swift
+++ GitZhuo/Hello.swift
@@ -5,3 +5,17 @@
 //  Created by Student on 4/23/26.
 //
 
+import SwiftUI
+
+struct Hello: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    Hello()
+}
